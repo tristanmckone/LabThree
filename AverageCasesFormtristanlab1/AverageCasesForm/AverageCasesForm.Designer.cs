640153169d4e0a5254f9552ca06eff369c468f0e
tristanmckone <85632890+tristanmckone@users.noreply.github.com>
2021-06-26 14:58:07 -0400

    Add files via upload

---
+++ AverageCasesFormtristanlab1/AverageCasesForm/AverageCasesForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace AverageCasesForm
+{
+    partial class formAverageCases
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelCasePrompt = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.labelAverageOutput = new System.Windows.Forms.Label();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBoxCaseEntry = new System.Windows.Forms.TextBox();
+            this.textBoxRegion1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.labelRegion1 = new System.Windows.Forms.Label();
+            this.labelRegion1Average = new System.Windows.Forms.Label();
+            this.labelRegion2 = new System.Windows.Forms.Label();
+            this.textBoxRegion2 = new System.Windows.Forms.TextBox();
+            this.labelRegion2Average = new System.Windows.Forms.Label();
+            this.labelRegion3 = new System.Windows.Forms.Label();
+            this.textBoxRegion3 = new System.Windows.Forms.TextBox();
+            this.labelRegion3Average = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelCasePrompt
+            // 
+            this.labelCasePrompt.AutoSize = true;
+            this.labelCasePrompt.Location = new System.Drawing.Point(56, 46);
+            this.labelCasePrompt.Name = "labelCasePrompt";
+            this.labelCasePrompt.Size = new System.Drawing.Size(72, 15);
+            this.labelCasePrompt.TabIndex = 0;
+            this.labelCasePrompt.Text = " Daily &Cases:";
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Location = new System.Drawing.Point(474, 39);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(33, 15);
+            this.labelDay.TabIndex = 2;
+            this.labelDay.Text = "Day1";
+            this.labelDay.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelAverageOutput
+            // 
+            this.labelAverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAverageOutput.Location = new System.Drawing.Point(31, 320);
+            this.labelAverageOutput.Name = "labelAverageOutput";
+            this.labelAverageOutput.Size = new System.Drawing.Size(523, 29);
+            this.labelAverageOutput.TabIndex = 14;
+            this.labelAverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(56, 370);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(127, 35);
+            this.buttonEnter.TabIndex = 13;
+            this.buttonEnter.Text = "&Enter";
+            this.toolTip1.SetToolTip(this.buttonEnter, "button to enter on program");
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.EnterClick);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(206, 372);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(123, 34);
+            this.buttonReset.TabIndex = 15;
+            this.buttonReset.Text = "&Reset";
+            this.toolTip1.SetToolTip(this.buttonReset, "button to reset program");
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ResetClick);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(369, 371);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(138, 33);
+            this.buttonExit.TabIndex = 16;
+            this.buttonExit.Text = "E&xit";
+            this.toolTip1.SetToolTip(this.buttonExit, "button to exit program");
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.ExitClick);
+            // 
+            // textBoxCaseEntry
+            // 
+            this.textBoxCaseEntry.Location = new System.Drawing.Point(145, 43);
+            this.textBoxCaseEntry.Name = "textBoxCaseEntry";
+            this.textBoxCaseEntry.Size = new System.Drawing.Size(244, 23);
+            this.textBoxCaseEntry.TabIndex = 1;
+            // 
+            // textBoxRegion1
+            // 
+            this.textBoxRegion1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxRegion1.Location = new System.Drawing.Point(51, 116);
+            this.textBoxRegion1.Multiline = true;
+            this.textBoxRegion1.Name = "textBoxRegion1";
+            this.textBoxRegion1.ReadOnly = true;
+            this.textBoxRegion1.Size = new System.Drawing.Size(132, 159);
+            this.textBoxRegion1.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.textBoxRegion1, "List of daily cases region1");
+            // 
+            // labelRegion1
+            // 
+            this.labelRegion1.AutoSize = true;
+            this.labelRegion1.Location = new System.Drawing.Point(56, 91);
+            this.labelRegion1.Name = "labelRegion1";
+            this.labelRegion1.Size = new System.Drawing.Size(53, 15);
+            this.labelRegion1.TabIndex = 3;
+            this.labelRegion1.Text = "Region &1";
+            this.labelRegion1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelRegion1Average
+            // 
+            this.labelRegion1Average.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRegion1Average.Location = new System.Drawing.Point(51, 284);
+            this.labelRegion1Average.Name = "labelRegion1Average";
+            this.labelRegion1Average.Size = new System.Drawing.Size(125, 25);
+            this.labelRegion1Average.TabIndex = 5;
+            this.labelRegion1Average.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelRegion1Average, "Average cases for region 1");
+            // 
+            // labelRegion2
+            // 
+            this.labelRegion2.AutoSize = true;
+            this.labelRegion2.Location = new System.Drawing.Point(231, 98);
+            this.labelRegion2.Name = "labelRegion2";
+            this.labelRegion2.Size = new System.Drawing.Size(53, 15);
+            this.labelRegion2.TabIndex = 7;
+            this.labelRegion2.Text = "Region &2";
+            this.labelRegion2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxRegion2
+            // 
+            this.textBoxRegion2.Location = new System.Drawing.Point(196, 116);
+            this.textBoxRegion2.Multiline = true;
+            this.textBoxRegion2.Name = "textBoxRegion2";
+            this.textBoxRegion2.Size = new System.Drawing.Size(132, 159);
+            this.textBoxRegion2.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.textBoxRegion2, "List of daily cases region2");
+            // 
+            // labelRegion2Average
+            // 
+            this.labelRegion2Average.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRegion2Average.Location = new System.Drawing.Point(196, 284);
+            this.labelRegion2Average.Name = "labelRegion2Average";
+            this.labelRegion2Average.Size = new System.Drawing.Size(125, 25);
+            this.labelRegion2Average.TabIndex = 9;
+            this.labelRegion2Average.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelRegion2Average, "Average cases for region 2");
+            // 
+            // labelRegion3
+            // 
+            this.labelRegion3.AutoSize = true;
+            this.labelRegion3.Location = new System.Drawing.Point(419, 98);
+            this.labelRegion3.Name = "labelRegion3";
+            this.labelRegion3.Size = new System.Drawing.Size(53, 15);
+            this.labelRegion3.TabIndex = 10;
+            this.labelRegion3.Text = "Region &3";
+            this.labelRegion3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxRegion3
+            // 
+            this.textBoxRegion3.Location = new System.Drawing.Point(350, 116);
+            this.textBoxRegion3.Multiline = true;
+            this.textBoxRegion3.Name = "textBoxRegion3";
+            this.textBoxRegion3.Size = new System.Drawing.Size(140, 159);
+            this.textBoxRegion3.TabIndex = 11;
+            this.toolTip1.SetToolTip(this.textBoxRegion3, "List of daily cases region3");
+            // 
+            // labelRegion3Average
+            // 
+            this.labelRegion3Average.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelRegion3Average.Location = new System.Drawing.Point(365, 284);
+            this.labelRegion3Average.Name = "labelRegion3Average";
+            this.labelRegion3Average.Size = new System.Drawing.Size(125, 25);
+            this.labelRegion3Average.TabIndex = 12;
+            this.labelRegion3Average.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelRegion3Average, "Average cases for region 3");
+            // 
+            // formAverageCases
+            // 
+            this.AcceptButton = this.buttonEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonReset;
+            this.ClientSize = new System.Drawing.Size(579, 450);
+            this.Controls.Add(this.labelRegion3Average);
+            this.Controls.Add(this.textBoxRegion3);
+            this.Controls.Add(this.labelRegion3);
+            this.Controls.Add(this.labelRegion2Average);
+            this.Controls.Add(this.textBoxRegion2);
+            this.Controls.Add(this.labelRegion2);
+            this.Controls.Add(this.labelRegion1Average);
+            this.Controls.Add(this.labelRegion1);
+            this.Controls.Add(this.textBoxRegion1);
+            this.Controls.Add(this.textBoxCaseEntry);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.labelAverageOutput);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelCasePrompt);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formAverageCases";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Average Weekly Units by Region";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCasePrompt;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Label labelAverageOutput;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox textBoxCaseEntry;
+        private System.Windows.Forms.TextBox textBoxRegion1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label labelRegion1;
+        private System.Windows.Forms.Label labelRegion1Average;
+        private System.Windows.Forms.Label labelRegion2;
+        private System.Windows.Forms.TextBox textBoxRegion2;
+        private System.Windows.Forms.Label labelRegion2Average;
+        private System.Windows.Forms.Label labelRegion3;
+        private System.Windows.Forms.TextBox textBoxRegion3;
+        private System.Windows.Forms.Label labelRegion3Average;
+    }
+}
+
---
+++ AverageCasesFormtristanlab1/AverageCasesForm/AverageCasesForm.cs
@@ -0,0 +1,192 @@
+﻿/// Tristan Mckone Lab
+/// 2021-06-26
+/// This program builds on lab 1 which takes an input of 7 days case total and then displays the average. 
+/// It does this 3 times then shows a total average of all 3.
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageCasesForm
+{
+    public partial class formAverageCases : Form
+    {
+        // Declare class level variables
+        int currentDay = 0;
+        int currentRegion = 0;
+        
+        const int NumberofRegions = 3;
+        const int NumberofDays = 7;
+        // 2D array of all recorded cases
+        int[,] cases = new int[NumberofRegions,NumberofDays];
+
+        // Arrays of controls of the form.
+        TextBox[] textBoxCaseLists;
+        Label[] labelAverages;
+        // intalizes the form and its controls into arrays.
+        public formAverageCases()
+        {
+            InitializeComponent();
+
+            textBoxCaseLists = new TextBox[] { textBoxRegion1, textBoxRegion2, textBoxRegion3 };
+            labelAverages = new Label[] { labelRegion1Average, labelRegion2Average, labelRegion3Average };
+        }
+        /// <summary>
+        /// This button exits the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+        /// <summary>
+        /// Clears all fields and resets the form to default.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ResetClick(object sender, EventArgs e)
+        {
+            SetDefaults();
+                    
+        }
+        /// <summary>
+        /// Accepts a value, when valid records and adds value to running total. Creates average after all cases are entered.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EnterClick(object sender, EventArgs e)
+        {
+            // Use constant for numbers of days.
+            const int NumberOfDays = 7;
+            // Store users last input in variable.
+            int enteredCases;
+
+            // If the textbox contains number, validate number(Range,positive).
+            if (int.TryParse(textBoxCaseEntry.Text, out enteredCases))
+            {
+                //If true, increment cases based on the input.
+                if (enteredCases >= 0 && enteredCases <= int.MaxValue)
+                {
+                    // Add entered cases into the array
+                    cases[currentRegion, currentDay] = enteredCases;
+                    // Add entered cases to the correct textbox
+                    textBoxCaseLists[currentRegion].Text += enteredCases + Environment.NewLine;
+                    // increment day.
+                    currentDay++;
+                    // When the day reaches 7, do calculation and go to next region.
+                    if (currentDay >= NumberOfDays)
+                    {
+                        // Calculate average for current region, for loop with i as counter.
+                        double totalCases = 0;
+                        for (int i = 0; i < NumberOfDays; i ++)
+                        {
+                            // Declare double value to hold number
+                            
+                            //Adds each total to a running total, as each day is passed in the current region.
+                            totalCases += cases[currentRegion, i];
+                        }
+                        // Use total to calculate and show average.
+                        labelAverages[currentRegion].Text = "Average: " + 
+                            Math.Round(totalCases / NumberOfDays,2);
+                        // Go to next region
+                        currentDay = 0;
+                        currentRegion++;
+                        // When region 3 is reached find average.
+                        if (currentRegion >= NumberofRegions)
+                        {
+                            // Calculate total for all regions
+                            //for each loop
+                            // use a constant for the number of weeks
+                            double overallCases = 0;
+                            double realOverallCases = 0;
+                            const double ThreeWeeks = 21;
+                            
+                            foreach (int j in cases)
+                            {
+
+                                overallCases += j;
+                                
+
+                            }
+
+                            // use the total to calculate and output overall average
+                            // round average to 2 decimal places
+                            // output average
+
+                            realOverallCases = Math.Round(overallCases/ThreeWeeks,2);
+                            
+
+                            labelAverageOutput.Text = realOverallCases.ToString();
+
+                            //disable the input textbox and calculate button.
+                            textBoxCaseEntry.Enabled = false;
+                            buttonEnter.Enabled = false;
+                        }
+                        labelDay.Text = "day " + currentDay + 1;
+                    }
+                    
+                }
+                // If contents not in range, show error.
+
+                else
+                {
+
+                    MessageBox.Show("Your entry must be between 0 and " + int.MaxValue, "Entry Error!");
+                    textBoxCaseEntry.SelectAll();
+                    textBoxCaseEntry.Focus();
+                }
+            }
+            // The contents is not a whole number, report an error.
+            else
+            {
+                MessageBox.Show("Your entry must be a whole number " , "Entry Error!");
+                textBoxCaseEntry.SelectAll();
+                textBoxCaseEntry.Focus();
+                
+                   
+            }
+        }
+        public void SetDefaults()
+        {
+            //Clear input and output controls
+            textBoxCaseEntry.Clear();
+            textBoxRegion1.Clear();
+            textBoxRegion2.Clear();
+            textBoxRegion3.Clear();
+            labelRegion1Average.Text = String.Empty;
+            labelRegion2Average.Text = String.Empty;
+            labelRegion3Average.Text = String.Empty;
+            labelAverageOutput.Text = String.Empty;
+
+            // enable any controls that could of been disabled
+            textBoxCaseEntry.Enabled = true;
+            buttonEnter.Enabled = true;
+
+            //Reset values of class level variables
+            currentDay = 0;
+            currentRegion = 0;
+            
+
+            
+
+            // Set day back to default
+            labelDay.Text = "Day " + currentDay + 1;
+
+
+            //Set focus
+            textBoxCaseEntry.Focus();
+        }
+    }
+
+}
